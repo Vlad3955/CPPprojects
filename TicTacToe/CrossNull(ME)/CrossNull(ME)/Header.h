8df673e8b973cc853f993d683a025374b66eb0fc
Vlad3955 <vladreut28@gmail.com>
2021-10-26 10:20:40 +0300

    Moved structs & enums into "Header",deleted them from "main" and "libs",include "header" in libs

--- TicTacToe/CrossNull(ME)/CrossNull(ME)/Header.h
+++ TicTacToe/CrossNull(ME)/CrossNull(ME)/Header.h
@@ -1,8 +1,49 @@
 #pragma once
-enum Cell;
-struct Coord;
-enum GameProgress;
-struct  Game;
+// Структуры и перечесления(struct & enum)
+// =====================================================================
+// =====================================================================
+
+// Перечисление ячеек(элементов) поля
+enum Cell : char// Таким образом обЪявляем чаровский тип
+{
+	CROSS = 'X',
+	ZERO = '0',
+	EMPTY = '_'
+};
+
+// Структура с координатами
+struct Coord
+{
+	size_t y{ 0 };
+	size_t x{ 0 };
+
+};
+
+// Перечисление состояний игры
+enum GameProgress
+{
+	IN_PROGRESS,
+	WON_HUMAN,
+	WON_AI,
+	DRAW
+};
+
+#pragma pack(push, 1)
+struct  Game
+{
+	Cell** ppField = nullptr;// указатель на указатель ячеек поля
+	const uint16_t SIZE = 5;// Поле 5х5 будет
+	Cell human;// Кто чем играет
+	Cell ai;// --//--
+	size_t turn = 0;// определение хода.Четный-человек, нечетный-ИИ
+	GameProgress progress = IN_PROGRESS;
+};
+#pragma pack(pop)
+
+//=======================================================================
+//=======================================================================
+
+// Функции
 void clearScr();
 void __fastcall initGame(Game& g);
 void __fastcall deinitGame(Game& g);
